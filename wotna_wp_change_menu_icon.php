8e273e41d0795eb3bcf8f84115e453dd89b9312b
Digireturn <58370096+digireturn@users.noreply.github.com>
2022-05-24 17:20:13 +0200

    Create wotna_wp_change_menu_icon.php

---
+++ wotna_wp_change_menu_icon.php
@@ -0,0 +1,9 @@
+function wotna_wp_change_menu_icon($slug,$icon_url=''){
+	global $menu;
+	foreach($menu as $order=>$m)
+		if($m[2]==$slug){
+			$menu[$order][6]=$icon_url;
+			return true;
+		}
+	return false;
+}
